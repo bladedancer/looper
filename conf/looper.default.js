0dc1bb44dda202b8e9d7fd61c79c33272d41a555
Gavin Matthews <gnatthews@axway.com>
2020-11-13 18:30:48 +0000

    Looper.

---
+++ conf/looper.default.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration used by the GreetFlow. 'saluation' is accessed via '$.helloworld.salutation'
+ * similarly any APIBuilder.config parameter can be accessed.
+ */
+module.exports = {
+	looper: {
+		url: 'http://localhost:8080/api'
+	}
+};
